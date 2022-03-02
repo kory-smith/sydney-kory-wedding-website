6d4c4fb72e5300d685309a0bfbe273bb615d6d99
Kory Smith <kor54e@gmail.com>
2022-03-02 16:11:19 -0500

    make the site how it was

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
@@ -7,15 +8,17 @@ import {
   ScrollRestoration,
 } from "remix";
 import styles from "./tailwind.css";
+import Navbar from "~/components/Navbar";
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
 
-export function links() {
+export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
-}
+};
+
 export default function App() {
   return (
     <html lang="en">
@@ -26,10 +29,12 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <Navbar />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <footer className="bg-pink-400 p-6 absolute bottom-0 w-full " />
       </body>
     </html>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,29 @@
 export default function Index() {
+  let weddingDay = new Date("9/10/2022");
+  let today = new Date(Date.now());
+  let difference = weddingDay.getTime() - today.getTime();
+  let daysTillWedding = Math.ceil(difference / (1000 * 3600 * 24));
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="p-8">
+      <h1 className="text-3xl py-2">Sydney and Kory's Wedding Website!</h1>
+      <h2 className="text-2xl">September 10, 2022—{daysTillWedding} days remaining</h2>
+      <br />
+      <p>
+        Ceremony at{" "}
+        <a href="https://goo.gl/maps/WC9LTFoP5mEeL1FE7" target="_blank">
+          St. William Church
+        </a>{" "}
+        @ 5:00 p.m.
+      </p>
+      <p>
+        Reception at{" "}
+        <a href="https://goo.gl/maps/usdDUJEhmsPDbead9" target="_blank">
+          The Cincinnati Club
+        </a>{" "}
+        @ 6:30 p.m. until 11:30 p.m.
+      </p>
+      <img src="engagement.jpeg" />
     </div>
   );
 }
