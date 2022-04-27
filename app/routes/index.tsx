8c295556fe776b6af8948779cb072d1aa3212d27
Kory Smith <kor54e@gmail.com>
2022-04-27 19:13:08 -0400

    more style tweaks

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,10 +5,11 @@ export default function Index() {
   let daysTillWedding = Math.ceil(difference / (1000 * 3600 * 24));
 
   return (
+    <div className="px-16 m-8">
       <h2 className="text-[#343337] text-5xl text-center p-5 m-8 border-2 border-[#343337] rounded shadow-lg">
         September 10, 2022—<span className="text-[#E3D8C6] display-block bg-[#343337] ml-2 p-2 rounded">{daysTillWedding}</span> days left!
       </h2>
-      <p>
+      <p className="text-3xl my-4">
         Ceremony at{" "}
         <a
           href="https://goo.gl/maps/WC9LTFoP5mEeL1FE7"
@@ -19,7 +20,7 @@ export default function Index() {
         </a>{" "}
         @ 5:00 p.m.
       </p>
-      <p>
+      <p className="text-3xl my-4">
         Reception at{" "}
         <a
           href="https://goo.gl/maps/usdDUJEhmsPDbead9"
@@ -31,7 +32,7 @@ export default function Index() {
         @ 6:30 p.m. until 11:30 p.m.
       </p>
       <img
-        className="border-solid border-2 border-indigo-600 align-middle"
+        className="mx-auto my-12"
         src="engagement.jpeg"
         alt="Kory proposing to Sydney"
       />
