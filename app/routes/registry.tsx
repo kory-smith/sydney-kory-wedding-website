b1cfc8066b94ca971d9cd21e21ddbf5cb803f4d9
Kory Smith <kor54e@gmail.com>
2022-05-01 18:01:07 -0400

    add a registry page

---
+++ app/routes/registry.tsx
@@ -0,0 +1,20 @@
+export default function Registry() {
+  return (
+    <div className="px-16 m-8 h-full">
+      <h1 className="text-3xl justify-self">Our Registries:</h1>
+			<br></br>
+      <a
+        href="https://www.amazon.com/wedding/kory-smith-sydney-vollmer-cincinnati-september-2022/registry/3N8WRFU8TLW7E
+		"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          className="object-scale-down border-2 hover:shadow-lg m-auto"
+          src="amazon wedding registry.png"
+          alt="Amazon wedding registry logo"
+        />
+      </a>
+    </div>
+  );
+}
