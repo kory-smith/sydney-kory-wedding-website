1f53b469b0046f427474e8e063e23544fb377b16
Kory Smith <kor54e@gmail.com>
2022-07-23 13:38:38 -0400

    add rsvp page to website

---
+++ app/routes/rsvp.tsx
@@ -0,0 +1,16 @@
+export default function RSVP() {
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSdd-ZWFLd2Lam5PCGrwiEEGiRvC1bS2bXumb4oj8OijFZ8ofA/viewform?embedded=true"
+      title="RSVP"
+      width="640"
+      height="1195"
+      frameBorder={0}
+      marginHeight={0}
+      marginWidth={0}
+      className="mx-auto"
+    >
+      Loading…
+    </iframe>
+  );
+}
