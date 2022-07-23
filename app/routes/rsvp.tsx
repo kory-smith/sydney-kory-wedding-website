7758cb02363f854f6f006459a99d35985219716e
Kory Smith <kor54e@gmail.com>
2022-07-23 13:43:25 -0400

    makes form element responsive

--- app/routes/rsvp.tsx
+++ app/routes/rsvp.tsx
@@ -1,16 +1,16 @@
 export default function RSVP() {
   return (
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSdd-ZWFLd2Lam5PCGrwiEEGiRvC1bS2bXumb4oj8OijFZ8ofA/viewform?embedded=true"
-      title="RSVP"
-      width="640"
-      height="1195"
-      frameBorder={0}
-      marginHeight={0}
-      marginWidth={0}
-      className="mx-auto"
-    >
-      Loading…
-    </iframe>
+    <div className="w-full h-full">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdd-ZWFLd2Lam5PCGrwiEEGiRvC1bS2bXumb4oj8OijFZ8ofA/viewform?embedded=true"
+        title="RSVP"
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
+        className="mx-auto h-full w-full"
+      >
+        Loading…
+      </iframe>
+    </div>
   );
 }
